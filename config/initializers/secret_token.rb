80b6bbe7ad1022686dadeb9e58cc4f89d2e0eecb
swong11 <sunwong11@gmail.com>
2015-06-22 08:33:28 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vireo::Application.config.secret_key_base = '47be7c6152020cede94c8ffe84927c4ac49f5164680f1c94b1bdd22df9f4ff40eb52cd89dd334cea7feb28f71c0313a90f87d9b0c885c7d3182c7eb61a84fcd1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vireo::Application.config.session_store :cookie_store, key: '_vireo_session'
